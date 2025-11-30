5e430d1d8322189e821b5399af140b7ef8c16a6d
BAIGUANGMEI <2868653801@qq.com>
2025-11-30 13:44:34 +0800

    feat:deploy api

--- frontend/cdsfrondend/src/api/axios.js
+++ frontend/cdsfrondend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8787', // 本地开发地址
+  baseURL: import.meta.env.VITE_API_URL || 'http://127.0.0.1:8787', // Local dev default
   headers: {
     'Content-Type': 'application/json',
   },
